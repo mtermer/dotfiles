aaa5ef0436efed745893bc215ed073f6fd3ad329
Michal Termer <mtermer@mini-m4.local>
2026-02-25 23:11:35 +0100

    backup orig files

--- install.sh
+++ install.sh
@@ -8,11 +8,16 @@ echo "--- Zahajuji instalaci dotfiles ---"
 # Seznam souborů k nalinkování (zdroj:cíl)
 files=("vimrc:.vimrc" "bashrc:.bashrc" "screenrc:.screenrc" "zshrc:.zshrc")
 
+DATE_EXT="$(date +%Y%m%d%H%M)"
+
 for item in "${files[@]}"; do
     src="${item%%:*}"
     dest="${item#*:}"
 
     echo "Linkuji $src -> ~/$dest"
+    [ -f "$HOME/$dest" ] && cp -p "$HOME/$dest"  "$HOME/${dest}.${DATE_EXT}"
+
+
     ln -sf "$DOTFILES_DIR/$src" "$HOME/$dest"
 done
 
